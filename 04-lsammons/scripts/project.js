7cde4faf475bf2c55e4b92163f4dfddeebe706b7
Laura Sammons <lsammons@gmail.com>
2016-03-20 14:26:56 -0700

    write logic for handlebar template, add date display / comp, add creatives field

--- 04-lsammons/scripts/project.js
+++ 04-lsammons/scripts/project.js
@@ -1,37 +1,23 @@
-var projects = []; 
+var projects = [];
 
 function Project (opts) {
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
+  this.client = opts.client;
+  this.clientUrl = opts.clientUrl;
   this.title = opts.title;
   this.category = opts.category;
   this.body = opts.body;
-  this.publishedOn = opts.publishedOn;
+  this.createdOn = opts.createdOn;
+  this.creatives = opts.creatives;
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
-  $newProject.removeClass('template');
-  if (!this.publishedOn) {
-    $newProject.addClass('draft');
-  }
-  $newProject.attr('data-category', this.category);
-  $newProject.attr('data-author', this.author)
-
-  $newProject.find('.byline a').html(this.author);
-  $newProject.find('.byline a').attr('href', this.authorUrl);
-  $newProject.find('h1:first').html(this.title);
-  $newProject.find('.article-body').html(this.body);
-  $newProject.find('time[pubdate]').attr('datetime', this.publishedOn)
-  $newProject.find('time[pubdate]').attr('title', this.publishedOn)
-  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
-  $newProject.append('<hr>');
-  return $newProject;
+  var myTemplate = $('#rawData-template').html();
+  var finishedTemplate = Handlebars.compile(myTemplate);
+  this.daysAgo = parseInt((new Date() - new Date(this.createdOn))/60/60/24/1000);
+  this.publishStatus = this.createdOn ? '- created ' + this.daysAgo + ' days ago' : '(comp/spec)';
+  this.creativeTeam = this.creatives;
+  return finishedTemplate(this);
 }
-
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
 
 rawData.forEach(function(ele) {
   projects.push(new Project(ele));
