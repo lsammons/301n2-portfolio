8d375b2c3ef067ff3af2829091e726a92342843d
Laura Sammons <lsammons@gmail.com>
2016-03-20 11:34:47 -0700

    starter files

---
+++ 04-lsammons/README.md
@@ -0,0 +1,19 @@
+# 04 Templates and Typography
+
+# User Stories: MVP
+
+1. As a developer, I want to use Handlebars for my project template, so that I can include new projects more easily.
+  - As a developer, I want my CSS styles to follow SMACSS organization, so that I know where to look for creating and editing styles.
+
+2. As a visitor, I want the site to use great typography, so that I have an enjoyable reading experience.
+
+#TODO:
+ 1. Set up your h1, h2, h3 elements according to a type scale.
+ 2. Include some good fonts, that work well together.
+ 3. Add Handlebars to your blogging system.
+ 4. Organize your CSS code according to SMACSS, with at least a file for base, layout, and modules.
+
+
+ ## TODO:
+   1. As the creator, I want to be able to indicate solo vs team projects in the title of the project. (Add rawData field)
+     - As the creator, I want the publish date to reflect months and years, not days.
---
+++ 04-lsammons/scripts/project.js
@@ -0,0 +1,42 @@
+var projects = []; 
+
+function Project (opts) {
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
+  if (!this.publishedOn) {
+    $newProject.addClass('draft');
+  }
+  $newProject.attr('data-category', this.category);
+  $newProject.attr('data-author', this.author)
+
+  $newProject.find('.byline a').html(this.author);
+  $newProject.find('.byline a').attr('href', this.authorUrl);
+  $newProject.find('h1:first').html(this.title);
+  $newProject.find('.article-body').html(this.body);
+  $newProject.find('time[pubdate]').attr('datetime', this.publishedOn)
+  $newProject.find('time[pubdate]').attr('title', this.publishedOn)
+  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+  $newProject.append('<hr>');
+  return $newProject;
+}
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele) {
+  projects.push(new Project(ele));
+})
+
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml())
+});
